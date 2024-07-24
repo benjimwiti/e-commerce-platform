3dc98430bf7b0b044197c1b1c8ad9dc46da13e64
Petunia675 <ogekepurity675@gmail.com>
2024-07-24 20:27:42 +0300

    product page frontend

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,14 @@
+// src/components/Footer.tsx
+import React from 'react'
+
+const Footer: React.FC = () => {
+  return (
+    <footer>
+      <div className="text-center mt-5 py-3">
+        All rights reserved. Amazona.
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
