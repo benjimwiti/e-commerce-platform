ea3bae793b8b909974676cf489f7872be5068318
Petunia675 <ogekepurity675@gmail.com>
2024-07-25 14:31:58 +0300

    cart functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,20 +3,48 @@
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage'; */
 import './App.css';
-import { Container, Nav, Navbar} from 'react-bootstrap';
+import { useContext, useEffect } from 'react'
+import { Badge, Button, Container, Nav, Navbar } from 'react-bootstrap'
+import { Link, Outlet } from 'react-router-dom'
+import { LinkContainer } from 'react-router-bootstrap'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { Store } from './Store'
+
 import Footer from './components/Footer';
-import { Outlet } from 'react-router-dom';
 
 function App() {
+  const {
+    state: { mode, cart },
+    dispatch,
+  } = useContext(Store)
+
+  useEffect(() => {
+    document.body.setAttribute('data-bs-theme', mode)
+  }, [mode])
+
+  const switchModeHandler = () => {
+    dispatch({ type: 'SWITCH_MODE' })
+  }
   return (
     <div className="d-flex flex-column vh-100">
+      <ToastContainer position="bottom-center" limit={1} />
       <header>
-        <Navbar bg="dark" variant='dark' expand="lg">
+        <Navbar expand="lg">
           <Container className='mt-3'>
-            <Navbar.Brand>PrimeStore</Navbar.Brand>
+          <LinkContainer to="/">
+              <Navbar.Brand>PrimeStore</Navbar.Brand>
+            </LinkContainer>
           </Container>
           <Nav>
-            <a href="/cart" className='nav-link'>Cart</a>
+          <Button variant={mode} onClick={switchModeHandler}>
+              <i className={mode === 'light' ? 'fa fa-sun' : 'fa fa-moon'}></i>
+            </Button>
+            <Link to="/cart" className='nav-link'>Cart  {cart.cartItems.length > 0 && (
+                <Badge pill bg="danger">
+                  {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                </Badge>
+              )}</Link>
             <a href='/signin' className='nav-link'>Sign In</a>
           </Nav>
         </Navbar>
